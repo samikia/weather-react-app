810f46232a2d2a1e1871e771755717a0366cc98e
samikia <kia.sami8@gmail.com>
2023-08-23 22:25:04 +0330

    remove console.log

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactAnimatedWeather from 'react-animated-weather';
 export default function WeatherIcon(props){
-    console.log(props.icon);
     const codeMapping = {
         "01d": "CLEAR_DAY",
         "01n": "CLEAR_NIGHT",
