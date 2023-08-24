59914f986fcf070dba12d01aaabf0d6600c55850
samikia <kia.sami8@gmail.com>
2023-08-24 22:39:28 +0330

    add forcast five days

--- src/weatherForcast.js
+++ src/weatherForcast.js
@@ -4,28 +4,33 @@ import "bootstrap/dist/css/bootstrap.css";
 import WeatherIcon from "./WeatherIcon";
 import WeatherForcastDay from "./weatherForcastDay";
 export default function WeatherForcast(props) {
-  let[loaded,setLoaded]=useState(false);
-  let[forcast,setForcast]=useState(null)
+  let [loaded, setLoaded] = useState(false);
+  let [forcast, setForcast] = useState(null);
   function handelResponse(response) {
     console.log(response);
-    setForcast(response.data.daily)
-    setLoaded(true)
-   }
-  if(loaded){
+    setForcast(response.data.daily);
+    setLoaded(true);
+  }
+  if (loaded) {
     console.log(forcast);
     return (
       <>
-        <div className="p2">
-          <div className="card h-100 rounded-pill">
-            <div className="card-body">
-             <WeatherForcastDay forcast={forcast[0]}/>
-            </div>
-          </div>
-        </div>
+        {forcast.map((dailyForcast, index) => {
+          if (index < 5) {
+            return (
+              <div className="p2">
+                <div className="card h-100 rounded-pill">
+                  <div className="card-body" key={index}>
+                    <WeatherForcastDay forcast={dailyForcast} />
+                  </div>
+                </div>
+              </div>
+            );
+          }
+        })}
       </>
     );
-  
-  }else{
+  } else {
     let key = "4b3503b2f08a729413c4d33ef1186004";
     let long = props.coords.lon;
     let lat = props.coords.lat;
@@ -33,5 +38,4 @@ export default function WeatherForcast(props) {
     axios.get(api).then(handelResponse);
     return null;
   }
-
 }
